d362de4f7928c94ddd2973c7f4e09147612e5e36
AndrewOctopus <andrewtbatty@gmail.com>
2025-01-28 13:33:05 -0500

    Dad said so

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.get("/die/all/humanity", (req, res) => {
+  res.json({ Die: " world" });
+});
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.post("/path", (req, res) => {
+  console.log(req.body);
+  res.json({ Die: fizzBuzz(req.body.numbers) });
+});
+
+
+app.put("/die/all/humanity", (req, res) => {
+  console.log(req.body);
+  res.json({ Die: req.body.name });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+function fizzBuzz(numbers) {
+    return numbers;
+}
+
